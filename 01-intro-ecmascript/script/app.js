adf35e2969a13c006448abae7b575408b175edf8
Mor4n <anm0ran@outlook.com>
2026-03-08 22:19:39 -0700

    Añadidos más lugares, mostrar la cantidad de personas, añadidos destinos dentro de méxico y cambio con llaves en if-else de descuentos, de mayor a menor descuento y ahora no son acumulables

--- 01-intro-ecmascript/script/app.js
+++ 01-intro-ecmascript/script/app.js
@@ -6,7 +6,7 @@ import { registrarDestino, mostrarItinerario } from "./viajes.js";
 // Iniciar la aplicación
 const iniciarApp = () => {
     // Ejemplo de cómo registrar destinos
-    registrarDestino("Paris", "2024-06-15", "Avión",2);
+    registrarDestino("Agua Prieta", "2024-06-15", "Avión",2);
     registrarDestino("Londres", "2024-07-01", "Tren",1);
 
     // Mostrar el itinerario de los viajes
--- 01-intro-ecmascript/script/viajes.js
+++ 01-intro-ecmascript/script/viajes.js
@@ -6,48 +6,51 @@ const destinos = [];
 
 // objeto de catalogo de destinos con precios
 const catalogoDestinos = {
-    "Paris":500,
-    "Londres":400,
-    "New York":600
+    "Paris": 500,
+    "Londres": 400,
+    "New York": 600,
+    "Agua Prieta": 50,
+    "Hermosillo": 150,
+    "Ciudad de México": 350,
 
 }
 
 // objeto de tipos de transporte
 const tiposTransporte = {
-    "Avión":200,
-    "Tren":100,
+    "Avión": 200,
+    "Tren": 100,
 }
 
 
 // Función para registrar un destino de viaje
-export const registrarDestino =(destino, fecha, transporte,cantidadPersonas) => {
+export const registrarDestino = (destino, fecha, transporte, cantidadPersonas) => {
     // TODO: Crear un objeto con los datos del destino
     const nuevoViaje = {
         destino: destino,
         fecha: fecha,
         transporte: transporte,
-        cantidadPersonas : cantidadPersonas,
-        costo: calcularCosto(destino, transporte,cantidadPersonas)
+        cantidadPersonas: cantidadPersonas,
+        costo: calcularCosto(destino, transporte, cantidadPersonas)
     };
 
     destinos.push(nuevoViaje);
 }
 
 // Función para calcular el costo del viaje
-const calcularCosto = (destino, transporte,cantidadPersonas) => {
+const calcularCosto = (destino, transporte, cantidadPersonas) => {
     let costoBase = 0;
     let costoTotal = 0;
 
     // Costo base por destino
-    
+
     costoBase = catalogoDestinos[destino]; // del objeto catalogoDestinos, obtengo el precio al buscar la clave que sea de destino
 
 
     // Costo adicional por tipo de transporte
 
     costoBase += tiposTransporte[transporte]; // igualmente, del objeto tipoTransporte obtengo el valor al encontrar el transporte coincidente, esto se suma al costo base
-   
-    costoTotal = costoBase*cantidadPersonas; // Calculo la cantidad total
+
+    costoTotal = costoBase * cantidadPersonas; // Calculo la cantidad total
 
     costoTotal = calcularDescuento(destino, costoTotal, cantidadPersonas) // Se aplica el descuento
 
@@ -63,6 +66,7 @@ export const mostrarItinerario = () => {
         console.log("Destino: " + viaje.destino);
         console.log("Fecha: " + viaje.fecha);
         console.log("Transporte: " + viaje.transporte);
+        console.log("Personas: " + viaje.cantidadPersonas);
         console.log("Costo: $" + viaje.costo);
         console.log("---------------------------");
     }
@@ -71,19 +75,36 @@ export const mostrarItinerario = () => {
 
 // Función para calcular el descuento
 
-const calcularDescuento = (destino,costoTotal, cantidadPersonas) =>{
+const calcularDescuento = (destino, costoTotal, cantidadPersonas) => {
 
     // arreglo de destinos en oferta
-    const ofertaDestinos = ["Paris","New York"]; 
+    const ofertaDestinos = ["Paris", "New York"];
 
-    // si el destino seleccionado se encuentra en el arreglo de destinos en oferta, que se le aplique un descuento del 5%
-    if (ofertaDestinos.includes(destino)) costoTotal = (costoTotal-(costoTotal*.05));
+    // arreglo de destinos en méxico
+    const destinosNacionales = ["Agua Prieta", "Hermosillo", "Ciudad de México"];
+
+    // si el destino seleccionado se encuentra en el arreglo de destinos en oferta, que se le aplique un descuento del 10%
+    if (ofertaDestinos.includes(destino)) {
+
+        return costoTotal = (costoTotal - (costoTotal * .10));
+
+    }
+
+    // si el destino es dentro del país que tenga un descuento adicional del 7%
+    else if (destinosNacionales.includes(destino)) {
+
+        return costoTotal = (costoTotal - (costoTotal * 0.07));
+
+    }
 
     // si son 2 personas o más, que haga un 5% de descuento adicional
-    if (cantidadPersonas >= 2) costoTotal = (costoTotal-(costoTotal*.05)); 
+    else if (cantidadPersonas >= 2) {
 
+        return costoTotal = (costoTotal - (costoTotal * .05));
+
+    }
     
 
-    // devuelvo el costoTotal con el descuento aplicado
+    // devuelvo el costoTotal si ninguno de los anteriores se cumple
     return costoTotal;
 }
