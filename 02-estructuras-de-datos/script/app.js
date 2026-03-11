bffb9c76318497d8aa67329f8afb15fa849f616a
Mor4n <anm0ran@outlook.com>
2026-03-11 04:16:15 -0700

    Añadido más o menos agregar producto y eliminar producto

---
+++ 02-estructuras-de-datos/script/app.js
@@ -0,0 +1,20 @@
+
+let listaDeCompras =[];
+
+// funcion que agrega un nuevo producto al final de la lista
+const agregarProducto=(producto) =>{
+    listaDeCompras.push(producto);
+};
+
+// funcion que agrega que elimina un producto de la lista.
+const eliminarProducto =(producto)=>{
+    
+    listaDeCompras.pop(producto);
+}
+
+// imprime todos los productos de la lista
+const mostrarLista = () =>{
+
+}
+
+//Asegúrate de que no haya productos duplicados en la lista.
