b849bfc5bedadf08d4b5105be9bbc1b980049e26
Mor4n <anm0ran@outlook.com>
2026-03-12 16:12:51 -0700

    Añadida la obtención de datos de un elemento al que le demos clic y creando un objeto a partir de él

--- 02-estructuras-de-datos/script/carrito.js
+++ 02-estructuras-de-datos/script/carrito.js
@@ -12,11 +12,29 @@ const agregarProducto = (e) =>{
     // console.log(`Hola desde ${e.target.classList}`);
     // que se ejecute lo siguiente solo cuando el producto que presionemos, tenga la clase de producto agregar ( o sea el botón, si presionamos la imagen o un texto del producto, que no pase nada)
     if(e.target.classList.contains("producto-agregar")){
-        console.log("Agregado");
+        const htmlProducto = e.target.parentElement; // de estar en el elemento botón, vamos atrás un nodo HTML y nos quedamos con el card-producto
+
+        extraerDatos(htmlProducto);
         
     }
 
 }
+
+// extraer info del producto
+
+const extraerDatos = (producto) => {
+    console.log(producto);
+
+    const producto_info = {
+        imagen: producto.querySelector("img").src,
+        nombre: producto.querySelector(".producto-titulo").textContent,
+        precio: producto.querySelector(".producto-precio").textContent,
+    }
+    console.log(producto_info);
+    
+
+}
+
 
 
 const iniciarEvents = () =>{
