c64c3e9753f622a326be0aa76f07aec9afa0f38d
Mor4n <anm0ran@outlook.com>
2026-03-11 19:15:16 -0700

    Añadido el eliminado de productos pero de cualquier tipo de producto con filter

--- 02-estructuras-de-datos/script/app.js
+++ 02-estructuras-de-datos/script/app.js
@@ -2,19 +2,46 @@
 let listaDeCompras =[];
 
 // funcion que agrega un nuevo producto al final de la lista
+//Asegúrate de que no haya productos duplicados en la lista.
+
 const agregarProducto=(producto) =>{
+    if(listaDeCompras.includes(producto)){
+        return console.log(`Producto "${producto}" de añadirse estaría duplicado, por lo tanto se omitirá`);
+    }
     listaDeCompras.push(producto);
+    console.log(`Añadido: ${producto}`);
+    
 };
 
 // funcion que agrega que elimina un producto de la lista.
 const eliminarProducto =(producto)=>{
+    // reemplazo el contenido del arreglo, dejo pasar todo elemento al arreglo excepto el producto que se quiere sacar
+    listaDeCompras = listaDeCompras.filter( elemento => elemento != producto);
+    console.log(`${producto} eliminado`);
     
-    listaDeCompras.pop(producto);
+
 }
 
 // imprime todos los productos de la lista
 const mostrarLista = () =>{
+    console.log("---Lista de productos---");
+    
+    listaDeCompras.forEach( producto => console.log(`Nombre: ${producto}`)
+    );
 
 }
 
-//Asegúrate de que no haya productos duplicados en la lista.
+
+agregarProducto("Atun");
+agregarProducto("Fresa");
+agregarProducto("Atun"); // No lo va a meter
+agregarProducto("Sandía");
+agregarProducto("Mandarina");
+
+mostrarLista();
+
+// eliminar producto
+
+eliminarProducto("Sandía");
+
+mostrarLista();
