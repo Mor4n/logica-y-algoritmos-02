aa1a0c20b340679dd5cbd82b96d80cdeb2934a96
Mor4n <anm0ran@outlook.com>
2026-03-12 16:01:03 -0700

    Añadido mini ejemplo de añadir producto (aunque solo es el presionar un botón)

--- 02-estructuras-de-datos/script/carrito.js
+++ 02-estructuras-de-datos/script/carrito.js
@@ -1,5 +1,29 @@
 
 
-const carrito = document.querySelector("#carrito");
+const lista_compras_contenedor = document.querySelector("#lista-compras-contenedor");
+const listado_productos = document.querySelector("#seccion-productos");
+const contenedor_productos = document.querySelector("#compras-tbody");
+const vaciar_lista = document.querySelector("#vaciar-lista");
 
-const listado_productos = document.querySelector("#seccion_productos")
+
+
+const agregarProducto = (e) =>{
+
+    // console.log(`Hola desde ${e.target.classList}`);
+    // que se ejecute lo siguiente solo cuando el producto que presionemos, tenga la clase de producto agregar ( o sea el botón, si presionamos la imagen o un texto del producto, que no pase nada)
+    if(e.target.classList.contains("producto-agregar")){
+        console.log("Agregado");
+        
+    }
+
+}
+
+
+const iniciarEvents = () =>{
+    
+    // a cada producto del listado, si se da clic, va a poderse agregar a la lista
+    listado_productos.addEventListener("click", agregarProducto);
+    
+}
+
+iniciarEvents();
