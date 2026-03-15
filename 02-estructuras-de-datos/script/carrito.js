b030e4a1501648bfaa62536bcc3c1d4e3a5ef9b2
Mor4n <anm0ran@outlook.com>
2026-03-15 02:45:36 -0700

    Añadido proyecto final anterior para complementarle la parte del carrito de compras

--- 02-estructuras-de-datos/script/carrito.js
+++ 02-estructuras-de-datos/script/carrito.js
@@ -1,7 +1,7 @@
 
 
 const lista_compras_contenedor = document.querySelector("#lista-compras-contenedor");
-const seccion_productos = document.querySelector("#seccion-productos");
+const catalogo_productos = document.querySelector(".catalogo-productos");
 const contenedor_productos = document.querySelector("#compras-tbody");
 const vaciar_lista = document.querySelector("#vaciar-lista");
 let listaDeCompras = [];
@@ -99,7 +99,7 @@ const obtenerProducto = (e) =>{
 
     // console.log(`Hola desde ${e.target.classList}`);
     // que se ejecute lo siguiente solo cuando el producto que presionemos, tenga la clase de producto agregar ( o sea el botón, si presionamos la imagen o un texto del producto, que no pase nada)
-    if(e.target.classList.contains("producto-agregar")){
+    if(e.target.classList.contains("producto-btn")){
         const htmlProducto = e.target.parentElement; // de estar en el elemento botón, vamos atrás un nodo HTML y nos quedamos con el card-producto
 
         extraerDatos(htmlProducto);
@@ -112,7 +112,7 @@ const obtenerProducto = (e) =>{
 const iniciarEvents = () =>{
     
     // a cada producto del listado, si se da clic, va a poderse agregar a la lista
-    seccion_productos.addEventListener("click", obtenerProducto);
+    catalogo_productos.addEventListener("click", obtenerProducto);
     contenedor_productos.addEventListener("click", eliminarProducto);
     vaciar_lista.addEventListener("click", () =>{
         // reseteo de arreglo
