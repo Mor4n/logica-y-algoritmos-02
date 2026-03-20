300b496a3ccf1a5f2b3d1feebf4c4bafaeb21935
Mor4n <anm0ran@outlook.com>
2026-03-19 22:41:47 -0700

    Añadido home para el github pages en general

---
+++ script/app.js
@@ -0,0 +1,13 @@
+
+const imagen = document.querySelectorAll(".card-img"); 
+
+imagen.forEach( elemento =>{
+
+    elemento.addEventListener("click",() =>{
+    
+        // alert("Clic")
+        
+    })
+
+
+})
