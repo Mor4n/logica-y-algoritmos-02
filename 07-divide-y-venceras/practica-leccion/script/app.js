352597fd4bda20faea371e8af5bf5d13dc4155dc
Mor4n <anm0ran@outlook.com>
2026-03-29 04:15:19 -0700

    Añadida la solución de findMax y base de HTML

--- 07-divide-y-venceras/practica-leccion/script/app.js
+++ 07-divide-y-venceras/practica-leccion/script/app.js
@@ -1,21 +1,26 @@
 function findMax(arr) {
-    // TODO: Agregar la condición del caso base
-    if (/* condición */) {
-        return /* valor */;
+    // Agregar la condición del caso base
+    if (arr.length===1) {
+        return arr[0];
     }
 
-    // TODO: Dividir el arreglo en dos mitades
-    const mid = /* calcular el punto medio */;
-    const left = /* obtener la primera mitad */;
-    const right = /* obtener la segunda mitad */;
+    // Dividir el arreglo en dos mitades
+    const mid = Math.floor(arr.length/2);
+    const left = arr.slice(0,mid);
+    const right = arr.slice(mid);
 
-    // TODO: Llamar recursivamente a la función para ambas mitades
-    const leftMax = /* llamada recursiva */;
-    const rightMax = /* llamada recursiva */;
+    console.log(`Mitad: ${mid} | Izquierda: ${left} | Derecha ${right}`);
+    
 
-    // TODO: Combinar las soluciones comparando los máximos
-    return /* máximo entre leftMax y rightMax */;
+    // Llamar recursivamente a la función para ambas mitades
+    const leftMax = findMax(left);
+    const rightMax = findMax(right);
+
+
+    // Combinar las soluciones comparando los máximos
+    return leftMax > rightMax ? leftMax : rightMax; 
 }
 // Ejemplo de entrada
 const numbers = [3, 8, 2, 10, 5, 7];
 console.log(findMax(numbers)); // Salida esperada: 10
+
