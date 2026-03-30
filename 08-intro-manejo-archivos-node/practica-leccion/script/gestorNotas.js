bf2cec1193c2a4526695cfc90ae96beee22f8c38
Mor4n <anm0ran@outlook.com>
2026-03-30 03:31:59 -0700

    Añadido eliminar nota y ejemplo, falta hacer un mini menu

--- 08-intro-manejo-archivos-node/practica-leccion/script/gestorNotas.js
+++ 08-intro-manejo-archivos-node/practica-leccion/script/gestorNotas.js
@@ -4,9 +4,9 @@ const fs = require("fs"); // importo modulo js
 const readline = require("readline"); // modulo tipo Console.ReadLine() o ReadKey de c sharp
 
 // configuración de readline
-const { stdin: input, stdout: output } = require('node:process');
-const { info } = require("node:console");
-const rl = readline.createInterface({ input, output });
+// const { stdin: input, stdout: output } = require('node:process');
+// const { info } = require("node:console");
+// const rl = readline.createInterface({ input, output });
 
 // rl.question("Ingrese el dato a mostrar: ", (respuesta)=>console.log(`Escribiste ${respuesta}`))
 
@@ -37,7 +37,7 @@ const agregarNota = (titulo, contenido)=>{
 
 const mostrarNotas = () => {
 
-  if (fs.existsSync(filePath)) {
+  if (fs.existsSync(ruta)) {
     
         let informacion_archivo = fs.readFileSync(ruta,"utf8");
         notas = JSON.parse(informacion_archivo);
@@ -49,3 +49,29 @@ const mostrarNotas = () => {
     console.log('No hay notas guardadas.');
   }
 }
+
+
+
+//? Eliminar nota
+
+const eliminarNota = (titulo) => {
+  if (fs.existsSync(ruta)) {
+
+    let informacion_archivo = fs.readFileSync(ruta,"utf-8");
+    notas = JSON.parse(informacion_archivo);
+
+    notas = notas.filter( elemento => elemento["titulo"]!==titulo);
+
+    fs.writeFileSync(ruta, JSON.stringify(notas));
+
+    console.log(`Nota con título "${titulo}" eliminada.`);
+  } else {
+    console.log('No hay notas para eliminar.');
+  }
+}
+
+//? Ejecución de ejemplo
+agregarNota('Compras', 'Comprar leche y pan.');
+mostrarNotas();
+eliminarNota('Compras');
+
