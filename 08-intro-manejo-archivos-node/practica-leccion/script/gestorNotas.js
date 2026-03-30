5e7190555a0207960c608027a5e7ab86237418b9
Mor4n <anm0ran@outlook.com>
2026-03-30 03:21:57 -0700

    Añadido agregar nota y mostrar notas

--- 08-intro-manejo-archivos-node/practica-leccion/script/gestorNotas.js
+++ 08-intro-manejo-archivos-node/practica-leccion/script/gestorNotas.js
@@ -1,10 +1,51 @@
 
+// importaciones
 const fs = require("fs"); // importo modulo js
-
 const readline = require("readline"); // modulo tipo Console.ReadLine() o ReadKey de c sharp
 
+// configuración de readline
 const { stdin: input, stdout: output } = require('node:process');
-
+const { info } = require("node:console");
 const rl = readline.createInterface({ input, output });
 
-rl.question("Ingrese el dato a mostrar: ", (respuesta)=>console.log(`Escribiste ${respuesta}`))
+// rl.question("Ingrese el dato a mostrar: ", (respuesta)=>console.log(`Escribiste ${respuesta}`))
+
+const ruta = "./notas.json";
+
+//? Agregar nota al archivo
+
+const agregarNota = (titulo, contenido)=>{
+    
+    let notas = [];
+
+    if( fs.existsSync(ruta) ){
+        let informacion_archivo = fs.readFileSync(ruta,"utf8");
+        notas = JSON.parse(informacion_archivo);
+
+    }
+
+    const nuevaNota = { titulo, contenido };
+    notas.push(nuevaNota);
+
+    fs.writeFileSync(ruta,JSON.stringify(notas));
+
+    console.log('Nota agregada correctamente');
+
+}
+
+//? Listar todas las notas que están guardadas
+
+const mostrarNotas = () => {
+
+  if (fs.existsSync(filePath)) {
+    
+        let informacion_archivo = fs.readFileSync(ruta,"utf8");
+        notas = JSON.parse(informacion_archivo);
+        notas.forEach( elemento => console.log(`${elemento["titulo"]}:  ${elemento["contenido"]}`));
+
+  
+    } 
+  else {
+    console.log('No hay notas guardadas.');
+  }
+}
